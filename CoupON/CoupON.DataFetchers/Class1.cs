9b35e40f836a2725a0bd3480a25a6ad7ac4700b3
ericblair <eric.t.blair@gmail.com>
2014-09-21 22:04:15 +0100

    Created initial solution structure
    Added projects the solution will require

---
+++ CoupON/CoupON.DataFetchers/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoupON.DataFetchers
+{
+    public class Class1
+    {
+    }
+}
