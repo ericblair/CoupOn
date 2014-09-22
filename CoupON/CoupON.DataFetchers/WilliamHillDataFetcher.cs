1501dfde4a3556206f5b114935b9c9dfb484c7f4
ericblair <eric.t.blair@gmail.com>
2014-09-22 22:05:52 +0100

    Created WH data fetcher
    Added class to return XML data feed for UK football match results

---
+++ CoupON/CoupON.DataFetchers/WilliamHillDataFetcher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+using CoupON.Interfaces.DataFetchers;
+
+namespace CoupON.DataFetchers
+{
+    public class WilliamHillDataFetcher : IWilliamHillDataFetcher
+    {
+        // TODO: Move this string to config file
+        private const string ukFootballMatchResultFeedUrl =
+            "http://cachepricefeeds.williamhill.com/openbet_cdn?action=template&template=getHierarchyByMarketType&classId=1&marketSort=MR&filterBIR=N";
+
+        public XDocument GetUkFootballMatchResultFeed()
+        {
+            var data = XDocument.Load(ukFootballMatchResultFeedUrl);
+
+            return data;
+        }
+    }
+}
